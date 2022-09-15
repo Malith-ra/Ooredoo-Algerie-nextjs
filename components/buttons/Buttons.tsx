85ec9e205c507b6cd843f61dc052aa325e97b9b7
Malith Ranasinghe <malith.ranasinghe@arimaclanka.com>
2022-09-15 10:29:11 +0530

    button component created

--- components/buttons/Buttons.tsx
+++ components/buttons/Buttons.tsx
@@ -4,10 +4,15 @@ import FilledButton from './Filled Button/FilledButton';
 const Buttons = () => {
   return (
     <>
-      <div className="w-full rounded overflow-hidden border bg-white ">
+      <div className="w-full rounded overflow-hidden border bg-white">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-9">Filled Button</div>
-          <FilledButton />
+          <FilledButton
+            className={
+              'text-white bg-red-100 rounded-4xl border-red-200 border-1 w-242 hover:opacity-80 font-medium  text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+            }
+            text="Register"
+          />
         </div>
       </div>
     </>
--- components/buttons/Filled Button/FilledButton.tsx
+++ components/buttons/Filled Button/FilledButton.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const FilledButton = () => {
+const FilledButton = ({ className, text }: any) => {
   return (
     <div>
-      <button
-        type="button"
-        className="text-white bg-red-100 rounded-4xl border-red-200 border-1 w-242 hover:opacity-90 focus:ring-4 focus:ring-red-100 font-medium  text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        Register
+      <button type="button" className={className}>
+        {text}
       </button>
     </div>
   );
