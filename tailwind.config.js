efbc99bceeaaaf39b81d74bea6a0f8fb76f8ffdb
Malith Ranasinghe <malith.ranasinghe@arimaclanka.com>
2022-09-17 23:42:30 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,50 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+
+//   theme: {
+//     extend: {
+//       colors: {
+//         // Configure your color palette here
+//         transparent: 'transparent',
+//         current: 'currentColor',
+//         white: '#ffffff',
+
+//         red: {
+//           100: '#ED1C24',
+//           200: '#D6001C',
+//         },
+//         // ...
+//       },
+//       fontFamily: {
+//         rubik: 'Rubik',
+//         NotoSans: 'Noto Sans',
+//       },
+//       borderRadius: {
+//         '4xl': '3rem',
+//       },
+//       borderWidth: {
+//         1: '1px',
+//       },
+//       width: {
+//         242: '242px;',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-
+  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        // Configure your color palette here
-        transparent: 'transparent',
-        current: 'currentColor',
-        white: '#ffffff',
+    extend: {},
+  },
 
-        red: {
-          100: '#ED1C24',
-          200: '#D6001C',
-        },
-        // ...
-      },
-      fontFamily: {
-        rubik: 'Rubik',
-        NotoSans: 'Noto Sans',
-      },
-      borderRadius: {
-        '4xl': '3rem',
-      },
-      borderWidth: {
-        1: '1px',
-      },
-      width: {
-        242: '242px;',
-      },
-    },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
